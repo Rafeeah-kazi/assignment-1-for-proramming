1adbf20cf7ff4264133306ccf73de105ad09d141
Rafeeah-kazi <morningbreezeasp.uae@gmail.com>
2025-11-02 05:08:22 -0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,8 +1,9 @@
-# Let's make a cool sentence!
+# excersice 1
 
 word1 = "Coding"
 word2 = "is"
 word3 = "Cool"
 
-# Join the words together with spaces and show the result
-print(word1 + word2 + word3)
+# Join the words together with space  and show the result
+
+print(word1 + " " +  word2 + " " + word3)
